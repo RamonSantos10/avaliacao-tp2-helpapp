3cf03336b9747217cc73b7d879ad8692734acca4
RamonSantos10 <ramontedescodossantos2020@gmail.com>
2025-04-22 23:29:38 -0300

    refactor: Ajustando Migrations e String de Conexão do Banco de Dados

--- HelpApp/HelpApp.Infra.Data/Migrations/20250423021913_Initial.Designer.cs
+++ HelpApp/HelpApp.Infra.Data/Migrations/20250423021913_Initial.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HelpApp.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250423001640_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20250423021913_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
